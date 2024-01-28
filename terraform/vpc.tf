6e2c376069df282185d20bcae9a18875c8dd116d
sriram10412 <44442307+sriram10412@users.noreply.github.com>
2024-01-28 15:03:27 +0800

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,27 +1,7 @@
-resource "google_compute_network" "NAME" {
-  name                    = "NAME"
+resource "google_compute_network" "vpc" {
+  name                    = "my_first_vpc"
   routing_mode            = "REGIONAL"
-  auto_create_subnetworks = false
+  auto_create_subnetworks = true
 }
-
-resource "google_compute_subnetwork" "NAME" {
-  name                     = 
-  ip_cidr_range            = 
-  network                  = 
-  private_ip_google_access = true
-  region                   = 
-
-  secondary_ip_range {
-    range_name    = 
-    ip_cidr_range = 
-  }
-
-  secondary_ip_range {
-    range_name    = 
-    ip_cidr_range = 
-  }
-}
-
-
 
 ###Aditional configuration candidate might fined usefull to add
