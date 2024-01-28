b54bafe4017c169aaafe23e57f83741418a6d990
sriram10412 <44442307+sriram10412@users.noreply.github.com>
2024-01-28 15:05:02 +0800

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,6 +1,22 @@
-resource "google_container_cluster" "helloweb3" {
+# Create a GKE cluster
+module "gke" {
+  source              = "terraform-google-modules/kubernetes-engine/google"
+  cluster_name        = "my-gke-cluster"
+  project_id          = "your-gcp-project-id"
+  region              = "us-central1"
+  network             = module.vpc.network_name
+  subnetwork          = module.vpc.subnetwork_name
+  ip_range_pods       = module.vpc.pod_ip_range
+  ip_range_services   = module.vpc.service_ip_range
+  enable_basic_auth   = false
+  enable_legacy_abac  = false
+  enable_network_policy = false
 
-}
-
-resource "google_container_node_pool" "app_pool" {
+  node_pools = {
+    default = {
+      machine_type = "n1-standard-2"
+      min_count    = 1
+      max_count    = 3
+    }
+  }
 }
