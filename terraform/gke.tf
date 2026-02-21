39445a909848797019fd74ce5c62ff5580e0331c
sriram10412 <44442307+sriram10412@users.noreply.github.com>
2026-02-22 02:03:53 +0800

    Create GKE cluster configuration file

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,22 +1,21 @@
-# Create a GKE cluster
-module "gke" {
-  source              = "terraform-google-modules/kubernetes-engine/google"
-  cluster_name        = "my-gke-cluster"
-  project_id          = "MY_FIRST_GCP"
-  region              = "asia-southeast1-c"
-  network             = module.vpc.network_name
-  subnetwork          = module.vpc.subnetwork_name
-  ip_range_pods       = module.vpc.pod_ip_range
-  ip_range_services   = module.vpc.service_ip_range
-  enable_basic_auth   = false
-  enable_legacy_abac  = false
-  enable_network_policy = false
+# Google Kubernetes Engine (GKE) Cluster Configuration
 
-  node_pools = {
-    default = {
-      machine_type = "n1-standard-2"
-      min_count    = 1
-      max_count    = 3
-    }
+provider "google" {
+  project = "<YOUR_PROJECT_ID>"
+  region  = "<YOUR_REGION>"
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "primary-cluster"
+  location = "<YOUR_ZONE>"
+
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "n1-standard-1"
   }
+}
+
+output "kubeconfig" {
+  value = google_container_cluster.primary.endpoint
 }
