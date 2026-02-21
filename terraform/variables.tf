9df57c91f913dc58fd150071d8b08ef1f8fa5192
sriram10412 <44442307+sriram10412@users.noreply.github.com>
2026-02-22 02:04:32 +0800

    Create terraform/variables.tf for Terraform variables

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+# Terraform Variables
+
+variable "example_variable" {
+  description = "An example variable"
+  type        = string
+  default     = "example_value"
+}
+
+# Add your other variables below this line.
